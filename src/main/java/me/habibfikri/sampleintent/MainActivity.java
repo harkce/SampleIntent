4b7a4a590740676ad7255966ff55d670007f2df2
harkce <habibgarut@gmail.com>
2015-12-31 18:32:49 +0700

    Fungsi button buat pindah intent

--- src/main/java/me/habibfikri/sampleintent/MainActivity.java
+++ src/main/java/me/habibfikri/sampleintent/MainActivity.java
@@ -1,7 +1,11 @@
 package me.habibfikri.sampleintent;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSub1 = (Button) findViewById(R.id.btn_activity_sub_1);
+        Button btnSub2 = (Button) findViewById(R.id.btn_activity_sub_2);
+        Button btnDial = (Button) findViewById(R.id.btn_activity_dial);
+
+        btnSub1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Sub1Activity.class));
+            }
+        });
+
+        btnSub2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Sub2Activity.class);
+                intent.putExtra("data", "Latihan buat intent");
+                startActivity(intent);
+            }
+        });
+
+        btnDial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse("tel:089617615727")));
+            }
+        });
     }
 }
