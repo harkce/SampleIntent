8c6e6cf6bea09cf66e25ea814000d9e10346f7e7
harkce <habibgarut@gmail.com>
2015-12-31 18:24:57 +0700

    Sub 1 activity

---
+++ src/main/java/me/habibfikri/sampleintent/Sub1Activity.java
@@ -0,0 +1,18 @@
+package me.habibfikri.sampleintent;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by habib on 31/12/15.
+ * Semangat!!
+ */
+public class Sub1Activity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_sub1);
+    }
+}
